c88db7832aa5a488d6336ae76a8707aff591e47e
Tim De jong <tide-jon@f1r3s10.codam.nl>
2025-03-13 21:57:10 +0100

    added tar.gz packed sourcecode

--- bongbong/src/main.c
+++ bongbong/src/main.c
@@ -9,7 +9,7 @@ int main(void) {
 #if PERFT
 	perft_run();
 #else
-	uci_run("MagnusFish", "vvan-der & tide-jon");
+	uci_run("MagnusFish (NNUE-420)", "vvan-der & tide-jon");
 #endif
 
 	return EXIT_SUCCESS;
